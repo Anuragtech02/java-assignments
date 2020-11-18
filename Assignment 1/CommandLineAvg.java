85b2f97471128ce94ff43781fbd6a9cf97c46d3f
Anuragtech02 <apal895@gmail.com>
2020-11-18 15:45:54 +0530

    Minor corrections in cmdAvg

---
+++ Assignment 1/CommandLineAvg.java
@@ -0,0 +1,17 @@
+class CommandLineAvg {
+    public static void main(String args[]) {
+
+        int sum = 0, ans = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            sum = sum + Integer.parseInt(args[i]);
+
+        }
+
+        ans = sum / args.length;
+
+        System.out.println("Average of arguments is " + ans);
+
+    }
+}
