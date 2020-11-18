4c64e591ff0c3069e3c639a40e3dd6bdc53461bc
akshatBcs <74649838+akshatBcs@users.noreply.github.com>
2020-11-18 14:52:01 +0530

    Create intMax.java

---
+++ intMax.java
@@ -0,0 +1,7 @@
+public class Demo {
+   public static void main(String[] args) {
+      System.out.println("Integer Datatype values...");
+      System.out.println("Min = " + Integer.MIN_VALUE);
+      System.out.println("Max = " + Integer.MAX_VALUE);
+}
+}
