acefe9601988f7da5e5488e893a67e3af18b7810
Anuragtech02 <apal895@gmail.com>
2020-11-18 15:11:03 +0530

    IntMax renamed, Armstrong number, MulTable done

--- Assignment 1/IntMax.java
+++ Assignment 1/IntMax.java
@@ -1,7 +1,7 @@
-public class intValue {
+public class IntMax {
    public static void main(String[] args) {
       System.out.println("Integer Datatype values...");
       System.out.println("Min = " + Integer.MIN_VALUE);
       System.out.println("Max = " + Integer.MAX_VALUE);
-}
+   }
 }
