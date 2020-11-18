bd08545ff39ae0399ae3d53b4e8f54195a42ce7f
akshatBcs <74649838+akshatBcs@users.noreply.github.com>
2020-11-18 14:55:22 +0530

    Delete intMax.java

--- intMax.java
+++
@@ -1,7 +0,0 @@
-public class intValue {
-   public static void main(String[] args) {
-      System.out.println("Integer Datatype values...");
-      System.out.println("Min = " + Integer.MIN_VALUE);
-      System.out.println("Max = " + Integer.MAX_VALUE);
-}
-}
