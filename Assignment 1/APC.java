7478b75e57c9c63e5564afe1d041b0d48a323eb2
akshatBcs <74649838+akshatBcs@users.noreply.github.com>
2020-11-18 14:15:22 +0530

    Update APC.java

--- Assignment 1/APC.java
+++ Assignment 1/APC.java
@@ -4,12 +4,12 @@ public class APC {
     final static double pi = 3.14;
 
     public static void main(String args[]) {
-        System.out.println("Area and Parameter of a circle");
+        System.out.println("Area and Perimeter of a circle");
         Scanner input = new Scanner(System.in);
         System.out.println("Enter radius of the circle : ");
         double radius = input.nextDouble();
         input.close();
         System.out.println("Area of the circle is : " + (pi * radius * radius));
-        System.out.println("Parameter of the circle is : " + (2 * pi * radius));
+        System.out.println("Perimeter of the circle is : " + (2 * pi * radius));
     }
 }
