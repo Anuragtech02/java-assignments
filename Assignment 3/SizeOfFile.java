be32b0d2128c19fd03634f690bf881dbe0051c3a
Adarsh <adarsh.hodadakatti@gmail.com>
2020-11-21 12:52:43 +0530

    sry

--- Assignment 3/SizeOfFile.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-public class SizeOfFile 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("Size of file OctalToDecimal.java is : " + new File("OctalToDecimal.java").length() + " bytes");
-        System.out.println("Size of file OctalToBinary.java is : " + new File("OctalToBinary.java").length() + " bytes");
-    }
-}
