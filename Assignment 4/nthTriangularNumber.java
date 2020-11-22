dd3d39e08c03c52149626c46230fb393ee8a0a73
Manas Tiwari <manastiwari28@gmail.com>
2020-11-23 00:12:36 +0530

    just one question left for anurag, everything done

---
+++ Assignment 4/nthTriangularNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class nthTriangularNumber {
+    public static int triangularNo(int n){
+        if(n == 0)
+            return 0;
+        
+        else if(n == 1)
+            return 1;
+        
+        else
+            return triangularNo(n-1) + n;
+    }
+           
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the nth no of which you want triangular number printed");
+        int index = sc.nextInt();
+
+        int tno = triangularNo(index);
+
+        System.out.println("The triangular number of " + index + " is "+ tno);
+        sc.close();
+    }
+    
+}
